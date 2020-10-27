d03c15d71e07167eb87f10a020f1c1232d781978
ffabdillah <faisalabdillahgithub@gmail.com>
2020-10-27 12:47:36 +0700

    update tugas

---
+++ ci4login/app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, harum! Qui facilis quasi nobis quibusdam eos obcaecati reiciendis voluptate, dicta facere, adipisci blanditiis dolore voluptatum in harum ipsam, repellendus nam.</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
